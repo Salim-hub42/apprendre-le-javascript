a0b061b70532ad6c3106132920f370aa4a8aad10
salim-hub42 <salim.khalfoun@yahoo.fr>
2025-03-07 13:05:27 +0100

    commit trois

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,20 +1,11 @@
 let canvas = document.getElementById('canvas1');
 let ctx = canvas.getContext('2d');
+let radial = ctx.createRadialGradient(200,100,20,200,100,75);
 
+radial.addColorStop(0, '#4C8');
+radial.addColorStop(0.25,'#48C');
+radial.addColorStop(1, '#A4A');
 ctx.beginPath();
-ctx.lineWidth = '5';
-ctx.fillStyle = '#4C8';
-ctx.arc(75, 100, 50, 0, Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.lineWidth = '5';
-ctx.fillStyle = '#A4A';
-ctx.arc(200, 100, 50, 0, Math.PI * 2);
-ctx.fill();
-
-ctx.beginPath();
-ctx.lineWidth = '5';
-ctx.fillStyle = '#48C';
-ctx.arc(325, 100, 50, Math.PI, 2*Math.PI);
+ctx.fillStyle = radial;
+ctx.arc(200,100,75,0,2*Math.PI);
 ctx.fill();
