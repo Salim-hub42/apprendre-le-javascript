02af1db9611ddab90e4ed7b33f1fdd7ce59c21a1
salim-hub42 <salim.khalfoun@yahoo.fr>
2025-03-10 15:39:16 +0100

    one by one

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);
+let bnt = document.getElementById('btn');
+let body = document.body;
+bnt.addEventListener('click', couleur);
+function couleur(){
+setTimeout((function(){body.style.backgroundColor = '#3BA'}), 200);
+setTimeout((function(){body.style.backgroundColor = '#4Ab'}), 400);
+setTimeout((function(){body.style.backgroundColor = '#59B'}), 600);
+setTimeout((function(){body.style.backgroundColor = '#66B'}), 800);
+setTimeout((function(){body.style.backgroundColor = '#63B'}), 1000);
+setTimeout((function(){body.style.backgroundColor = '83A'}), 1200);
+setTimeout((function(){body.style.backgroundColor = '#A3A'}), 1400);
+setTimeout((function(){body.style.backgroundColor = '#A59'}), 1600);
+setTimeout((function(){body.style.backgroundColor = '#A77'}), 1800);
+setTimeout((function(){body.style.backgroundColor = '#995'}), 2000);
 }
